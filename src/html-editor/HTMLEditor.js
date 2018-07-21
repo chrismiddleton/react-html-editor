4f0258bde9a1b2c05e2bd7943d0eef6c54635994
chrismiddleton <chrisdmiddleton@gmail.com>
2018-07-21 15:33:19 -0400

    all temp

---
+++ src/html-editor/HTMLEditor.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HTMLEditor extends React.Component {
+	render () {
+		return <textarea></textarea>;
+	}
+}
+
+export default HTMLEditor;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HTMLEditor from './html-editor/HTMLEditor.js'
+
+ReactDOM.render(
+	<HTMLEditor />
+);
